91ef883a46ed44adaf1a38321d5cbf62343796ad
Pallavi C <info@joindevops.com>
2025-04-16 22:40:14 +0530

    expense-infra

--- 80-cdn/data.tf
+++ 80-cdn/data.tf
@@ -6,6 +6,6 @@ data "aws_cloudfront_cache_policy" "CacheEnable" {
    name = "Managed-CachingOptimized"
 }
 
-data "aws_ssm_parameter" "https_certificate_arn" {
-  name  = "/${var.project_name}/${var.environment}/web_alb_certificate_arn"
-}
+# data "aws_ssm_parameter" "https_certificate_arn" {
+#   name  = "/${var.project_name}/${var.environment}/web_alb_certificate_arn"
+# }
