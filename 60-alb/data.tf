d134352dd3c4e3ed083148463478370ecdd2d88e
Pallavi C <info@joindevops.com>
2025-04-18 19:27:23 +0530

    expense-infra

--- 60-alb/data.tf
+++ 60-alb/data.tf
@@ -26,7 +26,7 @@ data "aws_ssm_parameter" "public_subnet_ids" {
 }
 
 data "aws_ssm_parameter" "ingress_alb_sg_id" {
-  name = "/${var.project_name}/${var.environment}/ingress_alb_sg_id"
+  name = "/${var.project_name}/${var.environment}-1/ingress_alb_sg_id"
 }
 
 data "aws_ssm_parameter" "https_certificate_arn" {
