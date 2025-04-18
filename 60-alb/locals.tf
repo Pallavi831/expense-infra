0bcbbd1df431d63c9806ab510742586bb1a88dd1
Pallavi C <info@joindevops.com>
2025-04-18 19:29:31 +0530

    expense-infra

--- 60-alb/locals.tf
+++ 60-alb/locals.tf
@@ -8,9 +8,17 @@
 # }
 
 
+# locals {
+#     resource_name = "${var.project_name}-${var.environment}"
+#     vpc_id = data.aws_ssm_parameter.vpc_id.value
+#     public_subnet_ids = split(",", data.aws_ssm_parameter.public_subnet_ids.value)
+#     https_certificate_arn = data.aws_ssm_parameter.https_certificate_arn.value
+# }
+
 locals {
-    resource_name = "${var.project_name}-${var.environment}"
-    vpc_id = data.aws_ssm_parameter.vpc_id.value
-    public_subnet_ids = split(",", data.aws_ssm_parameter.public_subnet_ids.value)
-    https_certificate_arn = data.aws_ssm_parameter.https_certificate_arn.value
+  resource_name           = "${var.project_name}-${var.environment}"
+  vpc_id                  = data.aws_ssm_parameter.vpc_id.value
+  public_subnet_ids       = split(",", data.aws_ssm_parameter.public_subnet_ids.value)
+  https_certificate_arn   = data.aws_ssm_parameter.https_certificate_arn.value
+  alb_ingress_sg_id       = data.aws_ssm_parameter.ingress_alb_sg_id.value
 }
