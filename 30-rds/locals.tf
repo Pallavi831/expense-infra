e42e00496b0849e8a9d9df47c786000b81f8d0d3
Pallavi C <info@joindevops.com>
2025-04-18 08:32:51 +0530

    expense-infra

--- 30-rds/locals.tf
+++ 30-rds/locals.tf
@@ -1,5 +1,5 @@
 locals {
   resource_name = "${var.project_name}-${var.environment}-1"
   mysql_sg_id = data.aws_ssm_parameter.mysql_sg_id.value
-  database_subnet_group_name = data.aws_ssm_parameter.database_subnet_group_name.value
+  database_subnet_group_name = aws_db_subnet_group.rds_subnet_group.name
 }
