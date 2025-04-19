8319bb5e8afc2f4a6504d817d504d32baae62dc8
Pallavi C <info@joindevops.com>
2025-04-19 22:26:09 +0530

    expense-infra

--- 60-alb/main.tf
+++ 60-alb/main.tf
@@ -1,87 +1,87 @@
-# Frontend ALB
-module "alb" {
-  source = "terraform-aws-modules/alb/aws"
-  internal = false
+# # Frontend ALB
+# module "alb" {
+#   source = "terraform-aws-modules/alb/aws"
+#   internal = false
 
-  name    = "${var.project_name}-${var.environment}-ingress-alb"
-  vpc_id  = data.aws_ssm_parameter.vpc_id.value
-  subnets = local.public_subnet_ids
-  create_security_group = false
-  security_groups = [data.aws_ssm_parameter.ingress_alb_sg_id.value]
-  enable_deletion_protection = false
+#   name    = "${var.project_name}-${var.environment}-ingress-alb"
+#   vpc_id  = data.aws_ssm_parameter.vpc_id.value
+#   subnets = local.public_subnet_ids
+#   create_security_group = false
+#   security_groups = [data.aws_ssm_parameter.ingress_alb_sg_id.value]
+#   enable_deletion_protection = false
 
-  tags = merge(
-    var.common_tags,
-    {
-      Name = "${var.project_name}-${var.environment}-ingress-alb"
-    }
-  )
-}
+#   tags = merge(
+#     var.common_tags,
+#     {
+#       Name = "${var.project_name}-${var.environment}-ingress-alb"
+#     }
+#   )
+# }
 
-resource "aws_lb_listener" "https" {
-  load_balancer_arn = module.alb.arn
-  port              = 443
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = local. https_certificate_arn # Make sure this ARN is correct
+# resource "aws_lb_listener" "https" {
+#   load_balancer_arn = module.alb.arn
+#   port              = 443
+#   protocol          = "HTTPS"
+#   ssl_policy        = "ELBSecurityPolicy-2016-08"
+#   certificate_arn   = local. https_certificate_arn # Make sure this ARN is correct
 
-  default_action {
-    type = "fixed-response"
-    fixed_response {
-      content_type = "text/html"
-      message_body = "<h1>Hello, I am from frontend web ALB with HTTPS</h1>"
-      status_code  = "200"
-    }
-  }
-}
+#   default_action {
+#     type = "fixed-response"
+#     fixed_response {
+#       content_type = "text/html"
+#       message_body = "<h1>Hello, I am from frontend web ALB with HTTPS</h1>"
+#       status_code  = "200"
+#     }
+#   }
+# }
 
-resource "aws_lb_target_group" "frontend" {
-  name             = local.resource_name
-  port             = 8080
-  protocol         = "HTTP"
-  vpc_id           = local.vpc_id
-  target_type      = "ip"
-  deregistration_delay = 60
+# resource "aws_lb_target_group" "frontend" {
+#   name             = local.resource_name
+#   port             = 8080
+#   protocol         = "HTTP"
+#   vpc_id           = local.vpc_id
+#   target_type      = "ip"
+#   deregistration_delay = 60
 
-  health_check {
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    timeout             = 5
-    protocol            = "HTTP"
-    port                = 8080
-    path                = "/"
-    matcher             = "200-299"
-    interval            = 10
-  }
-}
+#   health_check {
+#     healthy_threshold   = 2
+#     unhealthy_threshold = 2
+#     timeout             = 5
+#     protocol            = "HTTP"
+#     port                = 8080
+#     path                = "/"
+#     matcher             = "200-299"
+#     interval            = 10
+#   }
+# }
 
-resource "aws_lb_listener_rule" "frontend" {
-  listener_arn = aws_lb_listener.https.arn
-  priority     = 10
+# resource "aws_lb_listener_rule" "frontend" {
+#   listener_arn = aws_lb_listener.https.arn
+#   priority     = 10
 
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.frontend.arn
-  }
+#   action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.frontend.arn
+#   }
 
-  condition {
-    host_header {
-      values = ["expense-${var.environment}-1.${var.domain_name}"]
-    }
-  }
-}
+#   condition {
+#     host_header {
+#       values = ["expense-${var.environment}-1.${var.domain_name}"]
+#     }
+#   }
+# }
 
-resource "aws_route53_record" "web_alb" {
-  zone_id = var.zone_id
-  name    = "expense-${var.environment}-1.${var.domain_name}"
-  type    = "A"
+# resource "aws_route53_record" "web_alb" {
+#   zone_id = var.zone_id
+#   name    = "expense-${var.environment}-1.${var.domain_name}"
+#   type    = "A"
 
-  alias {
-    name                   = module.alb.dns_name
-    zone_id                = module.alb.zone_id
-    evaluate_target_health = false
-  }
-}
+#   alias {
+#     name                   = module.alb.dns_name
+#     zone_id                = module.alb.zone_id
+#     evaluate_target_health = false
+#   }
+# }
 
 # Ingress ALB (backend)
 module "ingress_alb" {
