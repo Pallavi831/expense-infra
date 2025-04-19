4eb59e00275bad85b2bacae4f59fcf7a839a5d01
Pallavi C <info@joindevops.com>
2025-04-19 22:35:27 +0530

    expense-infra

--- 60-alb/main.tf
+++ 60-alb/main.tf
@@ -83,51 +83,51 @@ resource "aws_route53_record" "web_alb" {
   }
 }
 
-# Ingress ALB (Backend)
-module "ingress_alb" {
-  source = "terraform-aws-modules/alb/aws"
+# # Ingress ALB (Backend)
+# module "ingress_alb" {
+#   source = "terraform-aws-modules/alb/aws"
 
-  internal = false
-  name     = "${local.resource_name}-ingress-alb"
-  vpc_id   = local.vpc_id
-  subnets  = local.public_subnet_ids
-  security_groups = [data.aws_ssm_parameter.ingress_alb_sg_id.value]
-  create_security_group = false
-  enable_deletion_protection = false
+#   internal = false
+#   name     = "${local.resource_name}-ingress-alb"
+#   vpc_id   = local.vpc_id
+#   subnets  = local.public_subnet_ids
+#   security_groups = [data.aws_ssm_parameter.ingress_alb_sg_id.value]
+#   create_security_group = false
+#   enable_deletion_protection = false
 
-  tags = merge(
-    var.common_tags
-  )
-}
+#   tags = merge(
+#     var.common_tags
+#   )
+# }
 
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = module.ingress_alb.arn
-  port              = 80
-  protocol          = "HTTP"
+# resource "aws_lb_listener" "http" {
+#   load_balancer_arn = module.ingress_alb.arn
+#   port              = 80
+#   protocol          = "HTTP"
 
-  default_action {
-    type = "fixed-response"
-    fixed_response {
-      content_type = "text/html"
-      message_body = "<h1>Hello, I am from Application ALB</h1>"
-      status_code  = "200"
-    }
-  }
-}
+#   default_action {
+#     type = "fixed-response"
+#     fixed_response {
+#       content_type = "text/html"
+#       message_body = "<h1>Hello, I am from Application ALB</h1>"
+#       status_code  = "200"
+#     }
+#   }
+# }
 
-resource "aws_lb_listener" "https" {
-  load_balancer_arn = module.ingress_alb.arn
-  port              = 443
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = local.https_certificate_arn # Ensure correct certificate ARN
+# resource "aws_lb_listener" "https" {
+#   load_balancer_arn = module.ingress_alb.arn
+#   port              = 443
+#   protocol          = "HTTPS"
+#   ssl_policy        = "ELBSecurityPolicy-2016-08"
+#   certificate_arn   = local.https_certificate_arn # Ensure correct certificate ARN
 
-  default_action {
-    type = "fixed-response"
-    fixed_response {
-      content_type = "text/html"
-      message_body = "<h1>Hello, I am from Web ALB HTTPS</h1>"
-      status_code  = "200"
-    }
-  }
-}
+#   default_action {
+#     type = "fixed-response"
+#     fixed_response {
+#       content_type = "text/html"
+#       message_body = "<h1>Hello, I am from Web ALB HTTPS</h1>"
+#       status_code  = "200"
+#     }
+#   }
+# }
