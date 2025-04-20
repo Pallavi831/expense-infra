86b8b845827d54a292f82069eac095bcf44e7b8c
Pallavi C <info@joindevops.com>
2025-04-20 20:42:11 +0530

    expense-infra

--- 60-alb/locals.tf
+++ 60-alb/locals.tf
@@ -1,7 +1,8 @@
 locals {
-  resource_name           = "${var.project_name}-${var.environment}"
-  vpc_id                  = data.aws_ssm_parameter.vpc_id.value
-  public_subnet_ids       = split(",", data.aws_ssm_parameter.public_subnet_ids.value)
-  https_certificate_arn   = data.aws_ssm_parameter.https_certificate_arn.value
-  alb_ingress_sg_id       = data.aws_ssm_parameter.ingress_alb_sg_id.value
+    # stringlist to list
+  public_subnet_ids = split(",", data.aws_ssm_parameter.public_subnet_ids.value)
+  alb_ingress_sg_id = data.aws_ssm_parameter.alb_ingress_sg_id.value
+  ingress_alb_certificate_arn = data.aws_ssm_parameter.ingress_alb_certificate_arn.value
+  resource_name = "${var.project_name}-${var.environment}-1"
+  vpc_id = data.aws_ssm_parameter.vpc_id.value
 }
--- 60-alb/main.tf
+++ 60-alb/main.tf
@@ -1,58 +1,55 @@
-# Frontend ALB (Web ALB)
 module "alb" {
   source = "terraform-aws-modules/alb/aws"
   internal = false
 
-  name    = "${var.project_name}-${var.environment}-1-ingress-alb"
+  # expense-dev-app-alb
+  name    = "${var.project_name}-${var.environment}-ingress-alb"
   vpc_id  = data.aws_ssm_parameter.vpc_id.value
   subnets = local.public_subnet_ids
   create_security_group = false
-  security_groups = [data.aws_ssm_parameter.ingress_alb_sg_id.value]
+  security_groups = [local.alb_ingress_sg_id]
   enable_deletion_protection = false
 
   tags = merge(
     var.common_tags,
     {
-      Name = "${var.project_name}-${var.environment}-1-ingress-alb"
+        Name = "${var.project_name}-${var.environment}-ingress-alb"
     }
   )
+  
 }
+
+
 
 resource "aws_lb_listener" "https" {
   load_balancer_arn = module.alb.arn
-  port              = 443
+  port              = "443"
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = local.https_certificate_arn # Ensure correct certificate ARN
+  certificate_arn   = local.ingress_alb_certificate_arn
 
   default_action {
-    type = "fixed-response"
+    type             = "fixed-response"
+    
     fixed_response {
       content_type = "text/html"
-      message_body = "<h1>Hello, I am from frontend web ALB with HTTPS</h1>"
+      message_body = "<h1> Hello, I am from frontend web ALB with HTTPS</h1>"
       status_code  = "200"
     }
   }
 }
 
-resource "aws_lb_target_group" "frontend" {
-  name             = local.resource_name
-  port             = 8080
-  protocol         = "HTTP"
-  vpc_id           = local.vpc_id
-  target_type      = "ip"
-  deregistration_delay = 60
+resource "aws_route53_record" "web_alb" {
+  zone_id = var.zone_id
+  name    = "expense-${var.environment}-1.${var.domain_name}"
+  type    = "A"
 
-  health_check {
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    timeout             = 5
-    protocol            = "HTTP"
-    port                = 8080
-    path                = "/"
-    matcher             = "200-299"
-    interval            = 10
-  }
+ # these are ALB DNS name and zone information
+  alias   {
+    name                   = module.alb.dns_name
+    zone_id                = module.alb.zone_id
+    evaluate_target_health = false
+    }
 }
 
 resource "aws_lb_listener_rule" "frontend" {
@@ -66,68 +63,28 @@ resource "aws_lb_listener_rule" "frontend" {
 
   condition {
     host_header {
-      values = ["expense-${var.environment}.${var.domain_name}"]
+      values = ["expense-${var.environment}-1.${var.domain_name}"]
     }
   }
 }
 
-resource "aws_route53_record" "web_alb" {
-  zone_id = var.zone_id
-  name    = "expense-${var.environment}.${var.domain_name}"
-  type    = "A"
+resource "aws_lb_target_group" "frontend" {
+  name     = local.resource_name
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = local.vpc_id
+  deregistration_delay = 60
+  target_type = "ip"
 
-  alias {
-    name                   = module.alb.dns_name
-    zone_id                = module.alb.zone_id
-    evaluate_target_health = false
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 5
+    protocol = "HTTP"
+    port = 8080
+    path = "/"
+    matcher = "200-299"
+    interval = 10
   }
+
 }
-
-# # Ingress ALB (Backend)
-# module "ingress_alb" {
-#   source = "terraform-aws-modules/alb/aws"
-
-#   internal = false
-#   name     = "${local.resource_name}-ingress-alb"
-#   vpc_id   = local.vpc_id
-#   subnets  = local.public_subnet_ids
-#   security_groups = [data.aws_ssm_parameter.ingress_alb_sg_id.value]
-#   create_security_group = false
-#   enable_deletion_protection = false
-
-#   tags = merge(
-#     var.common_tags
-#   )
-# }
-
-# resource "aws_lb_listener" "http" {
-#   load_balancer_arn = module.ingress_alb.arn
-#   port              = 80
-#   protocol          = "HTTP"
-
-#   default_action {
-#     type = "fixed-response"
-#     fixed_response {
-#       content_type = "text/html"
-#       message_body = "<h1>Hello, I am from Application ALB</h1>"
-#       status_code  = "200"
-#     }
-#   }
-# }
-
-# resource "aws_lb_listener" "https" {
-#   load_balancer_arn = module.ingress_alb.arn
-#   port              = 443
-#   protocol          = "HTTPS"
-#   ssl_policy        = "ELBSecurityPolicy-2016-08"
-#   certificate_arn   = local.https_certificate_arn # Ensure correct certificate ARN
-
-#   default_action {
-#     type = "fixed-response"
-#     fixed_response {
-#       content_type = "text/html"
-#       message_body = "<h1>Hello, I am from Web ALB HTTPS</h1>"
-#       status_code  = "200"
-#     }
-#   }
-# }
