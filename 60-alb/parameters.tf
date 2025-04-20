678d6e916317f9b53b102e80de4a91bb78ff695c
Pallavi C <info@joindevops.com>
2025-04-20 21:01:18 +0530

    expense-infra

--- 60-alb/parameters.tf
+++ 60-alb/parameters.tf
@@ -1,5 +1,11 @@
+# resource "aws_ssm_parameter" "web_alb_certificate_arn" {
+#   name  = "/${var.project_name}/${var.environment}/web_alb_certificate_arn"
+#   type  = "String"
+#   value = aws_lb_listener.https.arn
+# }
+
 resource "aws_ssm_parameter" "web_alb_certificate_arn" {
   name  = "/${var.project_name}/${var.environment}/web_alb_certificate_arn"
   type  = "String"
-  value = aws_lb_listener.https.arn
+  value = local.ingress_alb_certificate_arn  # <- Store the certificate ARN here, not the listener ARN
 }
