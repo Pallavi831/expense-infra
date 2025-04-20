38c43f8da3c50c5e60bdd4d974466e2cbd570186
Pallavi C <info@joindevops.com>
2025-04-20 20:47:31 +0530

    expense-infra

--- 80-cdn/data.tf
+++ 80-cdn/data.tf
@@ -1,19 +1,3 @@
-# data "aws_cloudfront_cache_policy" "noCache" {
-#    name = "Managed-CachingDisabled"
-# }
-
-# data "aws_cloudfront_cache_policy" "CacheEnable" {
-#    name = "Managed-CachingOptimized"
-# }
-
-# data "aws_ssm_parameter" "https_certificate_arn" {
-#   name  = "/${var.project_name}/${var.environment}/web_alb_certificate_arn"
-# }
-
-# data "aws_ssm_parameter" "https_certificate_arn" {
-#   name = "/${var.project_name}/${var.environment}/https_certificate_arn"
-# }
-
 
 data "aws_cloudfront_cache_policy" "noCache" {
    name = "Managed-CachingDisabled"
@@ -23,11 +7,6 @@ data "aws_cloudfront_cache_policy" "CacheEnable" {
    name = "Managed-CachingOptimized"
 }
 
-# Rename one of the duplicate data sources
-data "aws_ssm_parameter" "web_alb_certificate_arn" {
-  name  = "/${var.project_name}/${var.environment}/web_alb_certificate_arn"
-}
-
 data "aws_ssm_parameter" "https_certificate_arn" {
-  name = "/${var.project_name}/${var.environment}/https_certificate_arn"
+  name  = "/${var.project_name}/${var.environment}/web_alb_certificate_arn"
 }
